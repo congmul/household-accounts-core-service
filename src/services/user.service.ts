9adb952b01e77808797ce4f78992f3445efd8363
Jehyun Jung <congmul@hotmail.com>
2024-07-31 10:01:21 -0700

    update

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import config from "../config/config";
+import logger from "../utils/logger";
 
 export const userService = {
   checkExist: async (userId: string) => {
@@ -7,6 +8,8 @@ export const userService = {
       await axios.get(`${config.user_service_url}/user/check-exist/${userId}`);
       return true;
     } catch (err: any) {
+      logger.info("checkExist Error");
+      logger.error(err);
       return false;
     }
   },
